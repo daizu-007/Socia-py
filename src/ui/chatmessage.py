073209bb3ca140c5da7c4afefb098c78473e3fe5
daizu-007 <128668088+daizu-007@users.noreply.github.com>
2025-02-17 01:05:00 +0900

    文字色を黒に変更

--- src/ui/chatmessage.py
+++ src/ui/chatmessage.py
@@ -1,6 +1,7 @@
-import flet as ft
+# チャットのメッセージを表示するUI部品
+import flet
 
-class ChatMessage(ft.UserControl):
+class ChatMessage(flet.UserControl):
     def __init__(self, text, is_sender):
         super().__init__()
         self.text = text
@@ -8,20 +9,22 @@ class ChatMessage(ft.UserControl):
 
     def build(self):
         md_text = self.text
-        msg_container = ft.Container(
-            content=ft.Markdown(
+        msg_container = flet.Container(
+            content=flet.Markdown(
                 md_text,
                 selectable=True,
-                extension_set=ft.MarkdownExtensionSet.GITHUB_WEB,
-                md_style_sheet=ft.MarkdownStyleSheet.a_text_style,
+                extension_set=flet.MarkdownExtensionSet.GITHUB_WEB,
+                md_style_sheet=flet.MarkdownStyleSheet(
+                    p_text_style=flet.TextStyle(color=flet.colors.BLACK87, height=1.5),
+                )
             ),
-            border_radius=ft.border_radius.all(12),
-            padding=ft.padding.all(12),
-            bgcolor=ft.colors.BLUE_50 if self.is_sender else ft.colors.GREY_50,
-            border=ft.border.all(1, ft.colors.BLACK26),
+            border_radius=flet.border_radius.all(12),
+            padding=flet.padding.all(12),
+            bgcolor=flet.colors.BLUE_50 if self.is_sender else flet.colors.GREY_50,
+            border=flet.border.all(1, flet.colors.BLACK26),
         )
         if self.is_sender:
-            msg_container.alignment = ft.alignment.center_right
+            msg_container.alignment = flet.alignment.center_right
         else:
-            msg_container.alignment = ft.alignment.center_left
+            msg_container.alignment = flet.alignment.center_left
         return msg_container
